f981585a83267d0ca8fd3ec79776622fe88f921f
Simone-Domenici <domenici.simone00@gmail.com>
2024-11-07 14:46:02 +0100

    ADD function to open and close the overlay

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const postRow = document.querySelector('.post-row')
 const imgFocus = document.querySelector('.overlay-img-container img')
+const overlay = document.querySelector('.overlay')
+const htmlBody = document.querySelector('body')
 
 axios
 	.get('https://jsonplaceholder.typicode.com/photos', {
@@ -46,9 +48,22 @@ function openPostsImg(event) {
 	const clickedElement = event.target;
 	const clickedCard = clickedElement.closest('.card');
 
-  if (clickedCard) {
-    const clickedImageUrl = clickedCard.querySelector('.card-img img').src;
-	imgFocus.src = clickedImageUrl;
-	console.log(clickedImageUrl)
-  }
+  	if (clickedCard) {
+    	const clickedImageUrl = clickedCard.querySelector('.card-img img').src;
+		imgFocus.src = clickedImageUrl;
+		console.log(clickedImageUrl)
+  	}
+
+  	overlay.classList.toggle('d-none')
+  	htmlBody.classList.toggle('overflow-hidden')
 }  
+
+
+overlay.addEventListener('click', (event) => {
+	const clickedElement = event.target;
+	if (!clickedElement.classList.contains('overlay-img') && !clickedElement.classList.contains('overlay-img-container')) {
+		overlay.classList.add('d-none');
+		htmlBody.classList.remove('overflow-hidden');
+		console.log('no img')
+	}
+})
